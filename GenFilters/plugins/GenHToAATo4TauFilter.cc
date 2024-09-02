2b78166ac4a9fbb70df045efe1cb9d5c22eba8f4
bhbam <bbbam@crimson.ua.edu>
2024-09-02 16:45:42 -0500

    deltaR cut removed for signal

--- GenFilters/plugins/GenHToAATo4TauFilter.cc
+++ GenFilters/plugins/GenHToAATo4TauFilter.cc
@@ -49,35 +49,23 @@ bool GenHToAATo4TauFilter::filter(edm::StreamID, edm::Event& evt, const edm::Eve
 
   //Loop over all taus in Event
   unsigned HToTauTauCandidate = 0;
-  /*for (reco::GenParticleCollection::const_iterator iGen = genParticles->begin(); iGen != genParticles->end(); ++iGen) {
-    if ( abs(iGen->pdgId()) != 25 || iGen->numberOfDaughters() != 2 ) continue;
-    if ( abs(iGen->daughter(0)->pdgId()) != 15 || abs(iGen->daughter(1)->pdgId()) != 15 ) continue;
-    if ( iGen->daughter(0)->pt() < tauPtCut_ && iGen->daughter(1)->pt() < tauPtCut_ ) continue;
-    if ( iGen->daughter(0)->eta() > tauEtaCut_ || iGen->daughter(1)->eta() > tauEtaCut_ ) continue;
-    float deltaeta = fabs(iGen->daughter(0)->eta()-iGen->daughter(1)->eta());
-    float deltaphi = fabs(iGen->daughter(0)->phi()-iGen->daughter(1)->phi());
-    float deltaR = sqrt(deltaeta*deltaeta+deltaphi*deltaphi);
-    if ( deltaR > taudRCut_ ) continue;
-    ++HToTauTauCandidate;
-  }
-  return (HToTauTauCandidate >= nHiggs_);  //Return boolean whether event passes cut values*/
 //--------------------------------------------------------------------------------------------------------------------------------------
   // std::cout << " Applying GenHToAATo4TauFilter " << endl;
   for (reco::GenParticleCollection::const_iterator iGen = genParticles->begin(); iGen != genParticles->end(); ++iGen) {
     if ( abs(iGen->pdgId()) != 35 || iGen->numberOfDaughters() != 2 || iGen->daughter(0)->pdgId() != 25 || iGen->daughter(1)->pdgId() != 25 ) continue;
     if ( abs(iGen->daughter(0)->daughter(0)->pdgId()) != 15 || abs(iGen->daughter(0)->daughter(1)->pdgId()) != 15 || abs(iGen->daughter(1)->daughter(0)->pdgId()) != 15 || abs(iGen->daughter(1)->daughter(1)->pdgId()) != 15 ) continue;
-    // if ( (iGen->daughter(0)->daughter(0)->pt() < tauPtCut_ && iGen->daughter(0)->daughter(1)->pt() < tauPtCut_) || (iGen->daughter(1)->daughter(0)->pt() < tauPtCut_ && iGen->daughter(1)->daughter(1)->pt() < tauPtCut_ )) continue;
-    if ( (iGen->daughter(0)->daughter(0)->pt() < tauPtCut_ || iGen->daughter(0)->daughter(1)->pt() < tauPtCut_) || (iGen->daughter(1)->daughter(0)->pt() < tauPtCut_ || iGen->daughter(1)->daughter(1)->pt() < tauPtCut_ )) continue;
+    if ( (iGen->daughter(0)->daughter(0)->pt() < tauPtCut_ && iGen->daughter(0)->daughter(1)->pt() < tauPtCut_) || (iGen->daughter(1)->daughter(0)->pt() < tauPtCut_ && iGen->daughter(1)->daughter(1)->pt() < tauPtCut_ )) continue;
+    // if ( (iGen->daughter(0)->daughter(0)->pt() < tauPtCut_ || iGen->daughter(0)->daughter(1)->pt() < tauPtCut_) || (iGen->daughter(1)->daughter(0)->pt() < tauPtCut_ || iGen->daughter(1)->daughter(1)->pt() < tauPtCut_ )) continue;
     // if ( (abs(iGen->daughter(0)->daughter(0)->eta()) > tauEtaCut_ || abs(iGen->daughter(0)->daughter(1)->eta()) > tauEtaCut_) && (abs(iGen->daughter(1)->daughter(0)->eta()) > tauEtaCut_ || abs(iGen->daughter(1)->daughter(1)->eta()) > tauEtaCut_)) continue;
     if ( (abs(iGen->daughter(0)->daughter(0)->eta()) > tauEtaCut_ || abs(iGen->daughter(0)->daughter(1)->eta()) > tauEtaCut_) || (abs(iGen->daughter(1)->daughter(0)->eta()) > tauEtaCut_ || abs(iGen->daughter(1)->daughter(1)->eta()) > tauEtaCut_)) continue;
-    float deltaeta1 = fabs(iGen->daughter(0)->daughter(0)->eta()-iGen->daughter(0)->daughter(1)->eta());
-    float deltaphi1 = fabs(iGen->daughter(0)->daughter(0)->phi()-iGen->daughter(0)->daughter(1)->phi());
-    float deltaeta2 = fabs(iGen->daughter(1)->daughter(0)->eta()-iGen->daughter(1)->daughter(1)->eta());
-    float deltaphi2 = fabs(iGen->daughter(1)->daughter(0)->phi()-iGen->daughter(1)->daughter(1)->phi());
-
-    float deltaR1 = sqrt(deltaeta1*deltaeta1 + deltaphi1*deltaphi1);
-    float deltaR2 = sqrt(deltaeta2*deltaeta2 + deltaphi2*deltaphi2);
-    if ( deltaR1 > taudRCut_ || deltaR2 > taudRCut_) continue; // meged taus
+    // float deltaeta1 = fabs(iGen->daughter(0)->daughter(0)->eta()-iGen->daughter(0)->daughter(1)->eta());
+    // float deltaphi1 = fabs(iGen->daughter(0)->daughter(0)->phi()-iGen->daughter(0)->daughter(1)->phi());
+    // float deltaeta2 = fabs(iGen->daughter(1)->daughter(0)->eta()-iGen->daughter(1)->daughter(1)->eta());
+    // float deltaphi2 = fabs(iGen->daughter(1)->daughter(0)->phi()-iGen->daughter(1)->daughter(1)->phi());
+    //
+    // float deltaR1 = sqrt(deltaeta1*deltaeta1 + deltaphi1*deltaphi1);
+    // float deltaR2 = sqrt(deltaeta2*deltaeta2 + deltaphi2*deltaphi2);
+    // if ( deltaR1 > taudRCut_ || deltaR2 > taudRCut_) continue; // meged taus
     // if (!( deltaR1 > taudRCut_ && deltaR2 > taudRCut_)) continue; // unmerged taus
     // std::cout << " deltaR1:------- " << deltaR1 << "deltaR2:------- "<<deltaR2 << endl;
     // std::cout << " A1 mass:------- " << iGen->daughter(0).mass() << "A2 mass:------- "<<iGen->daughter(1).mass() << endl;
@@ -86,9 +74,9 @@ bool GenHToAATo4TauFilter::filter(edm::StreamID, edm::Event& evt, const edm::Eve
     std::cout<<"========================================================event passed filter======================================================="<<endl;
     std::cout << " eta1  : " << iGen->daughter(0)->daughter(0)->eta()<< " eta2  : "<< iGen->daughter(0)->daughter(1)->eta() <<" eta3  : "<< iGen->daughter(1)->daughter(0)->eta() <<" eta4  : "<< iGen->daughter(0)->daughter(1)->eta() << std::endl;
     std::cout << " status1  : " << iGen->daughter(0)->daughter(0)->status()<< " status2  : "<< iGen->daughter(0)->daughter(1)->status() <<" status3  : "<< iGen->daughter(1)->daughter(0)->status() <<" status4  : "<< iGen->daughter(0)->daughter(1)->status() << std::endl;
-    std::cout << "deltsR1  :   " << deltaR1 <<"            "<< "deltar2  :   " << deltaR2 << endl;
+    // std::cout << "deltsR1  :   " << deltaR1 <<"            "<< "deltar2  :   " << deltaR2 << endl;
   }
-  std::cout << "HToTauTauCandidate  :   " << HToTauTauCandidate << endl;
+  // std::cout << "HToTauTauCandidate  :   " << HToTauTauCandidate << endl;
   return (HToTauTauCandidate >= nHiggs_);  //Return boolean whether event passes cut values
 //---------------------------------------------------------------------------------------------------------------------------------------
 
